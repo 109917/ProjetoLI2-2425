17ef5a316567220b919c308273e2054e786a5526
guscm2 <155402699+guscm2@users.noreply.github.com>
2025-04-28 11:42:05 +0100

    meia entrega 2 feita

--- main.c
+++ main.c
@@ -3,6 +3,51 @@
 #include <string.h>
 
 #define TAMANHO 5
+#define MAXUNDO 100
+
+typedef struct estado {
+    char tabuleiro [TAMANHO][TAMANHO];
+    struct estado *anterior;
+} estado_t;
+
+estado_t *estado_atual = NULL;
+
+void salvar_estado() {
+    estado_t *novo = malloc(sizeof(estado_t));
+    memcpy(novo->tabuleiro, estado_atual->tabuleiro, sizeof(novo->tabuleiro));
+    novo->anterior = estado_atual;
+    estado_atual = novo;
+}
+
+void desfazer() {
+    if (estado_atual->anterior) {
+        estado_t *temp = estado_atual;
+        estado_atual = estado_atual->anterior;
+        free(temp);
+    } else {
+        printf("Não há mais estados para desfazer.\n");
+    }
+}
+
+void começar_jogo() {
+    estado_atual = malloc(sizeof(estado_t));
+
+    // Exemplo: preencher o tabuleiro com letras minúsculas
+    for (int i = 0; i < TAMANHO; i++) {
+        for (int j = 0; j < TAMANHO; j++) {
+            estado_atual->tabuleiro[i][j] = 'a' + (j % 26); // só como exemplo
+        }
+    }
+
+    estado_atual->anterior = NULL;
+}
+
+void salvar_estado() {
+    estado_t *novo = malloc(sizeof(estado_t));
+    memcpy(novo->tabuleiro, estado_atual->tabuleiro, sizeof(novo->tabuleiro));
+    novo->anterior = estado_atual;
+    estado_atual = novo;
+}
 
 typedef struct {
     int linha;
@@ -42,12 +87,9 @@ char casas[] = {'a', 'b', 'c', 'd', 'e', 'A', 'B', 'C', 'D', 'E', '#'};
 
 
 void printTabuleiro(){
-    for (int i = 0; i < TAMANHO; i++){
-        for (int j = 0; j < TAMANHO; j++){
-            if (tabuleiro[i][j] == 0){
-                printf(" ");
-            }
-            else printf ("%c", tabuleiro[i][j]);
+    for (int i = 0; i < TAMANHO; i++) {
+        for (int j = 0; j < TAMANHO; j++) {
+            printf("%c ", estado_atual->tabuleiro[i][j]);
         }
         printf("\n");
     }
@@ -86,23 +128,55 @@ void casaRiscada (int i, int j){
         }
 }
 
+void comandos(char *input) {
+    if (input[0] == 's') {
+        exit(0);
+    } else if (input[0] == 'g' && input[1] == ' ') {
+        gravar_estado(input + 2);
+    } else if (input[0] == 'l' && input[1] == ' ') {
+        ler_estado(input + 2);
+    } else if (input[0] == 'b' && input[1] == ' ') {
+        coordenada_t coord = parse_coord(input + 2);
+        casaaBranco(coord);
+    } else if (input[0] == 'r' && input[1] == ' ') {
+        coordenada_t coord = parse_coord(input + 2);
+        casaRiscada(coord);
+    } else if (input[0] == 'v') {
+        verificar_estado();
+    } else if (input[0] == 'a') {
+        ajuda();
+    } else if (input[0] == 'A') {
+        ajuda_iterativa();
+    } else if (input[0] == 'R') {
+        resolver_jogo();
+    } else if (input[0] == 'd') {
+        desfazer();
+    } else if (eh_coordenada(input)) {
+        coordenada_t coord = parse_coord(input);
+        selecionar_coord(coord);
+    } else {
+        printf("Comando desconhecido.\n");
+    }
+}
 
 int main () {
-    char input;
-    int i, j;
-    int c = 0;
+    começar_jogo();
+
+    char comando[100];
+
     while (1){
         printTabuleiro();
-        c = scanf(" %c %d %d", &input , &i, &j);
-        if (c == 1 && input == 's') break;
-        if (c == 3) {
-            if (input == 'b') casaaBranco(i, j);
-            else 
-                if (input == 'r') casaRiscada(i, j);
-        }
-        else {
-            printf("Comando invalido\n");
+        printf("> ");
+        fgets(comando, sizeof(comando), stdin);
+
+        if (comando[0] == 's') break;
+        else if (comando[0] == 'd') desfazer();
+        else if (comando[0] == 'b') {
+            int linha = comando[2] - '1';       // ex: b 3  → linha 2
+            int coluna = comando[1] - 'a';      // ex: b c  → coluna 2
+            pintar_maiuscula(linha, coluna);
         }
     }
+
     return 0;
 }
